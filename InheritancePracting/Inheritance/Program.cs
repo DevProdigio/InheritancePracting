01d8c0e23cecc9082d94d4a30e7bc69e7863ed50
Pedro Paulo Carneiro Ferreira <158389145+DevProdigio@users.noreply.github.com>
2025-02-03 19:14:34 -0300

    Add files via upload

---
+++ InheritancePracting/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Carro carro = new Carro { Marca = "Toyota", Ano = 2006, NumeroPortas = 4 };
+        Moto moto = new Moto { Marca = "Honda" , Ano = 2008, Cilindrada = 200};
+
+        carro.LigarMotor();
+        carro.AbrirPortas();
+
+        moto.LigarMotor();
+        moto.Empinar();
+    }
+}
---
+++ InheritancePracting/Inheritance/Veiculo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Veiculo
+    {
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+
+        public void LigarMotor()
+        {
+
+        }
+    }
+
+    class Carro : Veiculo
+    {
+        public int NumeroPortas { get; set; }
+
+        public void AbrirPortas()
+        {
+            Console.WriteLine($"Abrindo {NumeroPortas} Portas");
+        }
+    }
+
+    class Moto : Veiculo
+    {
+        public int Cilindrada { get; set; }
+
+        public void Empinar()
+        {
+            Console.WriteLine($"Moto Empinando com cilindrada de {Cilindrada} cc");
+        }
+    }
+}
+
